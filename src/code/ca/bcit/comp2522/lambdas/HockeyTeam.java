57ae22480bc134161022885554bb4608b008fc29
David Martinez <Dmartinez21@my.bcit.ca>
2025-10-10 08:18:48 -0700

    Fill in fields for team and setup main. Ready for lambda implementations

--- src/code/ca/bcit/comp2522/lambdas/HockeyTeam.java
+++ src/code/ca/bcit/comp2522/lambdas/HockeyTeam.java
@@ -1,5 +1,55 @@
 package ca.bcit.comp2522.lambdas;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a hockey team
+ * Has a team name (String)
+ * Has a team rooster (List)
+ *
+ * @author David Martinez
+ *         Daniel Do
+ * @version 1.0
+ */
 public class HockeyTeam
 {
+    private final String teamName;
+    private final List<HockeyPlayer> teamRooster;
+
+    /**
+     * Full constructor
+     *
+     * @param teamName This team's name
+     */
+    HockeyTeam(final String teamName,
+               final List<HockeyPlayer> teamRooster)
+    {
+        checkString(teamName);
+
+        this.teamName = teamName;
+        this.teamRooster = teamRooster;
+    }
+
+    /*
+    Check for null string and blankness, invalid if true
+    Throws new IllegalArgumentException
+     */
+    private void checkString(final String s)
+    {
+        if (s == null || s.isBlank())
+        {
+            throw new IllegalArgumentException("string cannot be null or blank");
+        }
+    }
+
+    /**
+     * Getter for team's roster
+     *
+     * @return the team roster list
+     */
+    public List<HockeyPlayer> getRoster()
+    {
+        return teamRooster;
+    }
 }
--- src/code/ca/bcit/comp2522/lambdas/Main.java
+++ src/code/ca/bcit/comp2522/lambdas/Main.java
@@ -1,5 +1,48 @@
 package ca.bcit.comp2522.lambdas;
 
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Main class for running tests for comp 2522 lab 6
+ *
+ * @author David Martinez
+ *         Daniel Do
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Returns a new sample team
+     *
+     * @return a new sample team
+     */
+    private static HockeyTeam sampleTeam() {
+        final List<HockeyPlayer> ps;
+        ps = new ArrayList<>();
+        ps.add(new HockeyPlayer("Alex Morgan", "F", 2002, 21));
+        ps.add(new HockeyPlayer("Ben Carter", "D", 1999, 6));
+        ps.add(new HockeyPlayer("Casey Young", "F", 2004, 28));
+        ps.add(new HockeyPlayer("Drew Singh", "G", 2000, 0));
+        ps.add(new HockeyPlayer("Eva Chen", "D", 2001, 5));
+
+        return new HockeyTeam("BCIT Blizzards", ps);
+    }
+
+    /**
+     * Runner class for Main
+     *
+     * @param args unused
+     */
+    public static void main(final String[] args)
+    {
+        final int currentYear = 2025;
+        final HockeyTeam team;
+        final List<HockeyPlayer> roster;
+
+        team = sampleTeam();
+        roster = team.getRoster();
+    }
+
+
 }
